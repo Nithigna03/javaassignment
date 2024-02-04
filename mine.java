175015a6469feb60289eb26eb5fb15782db2dc9b
Nithigna03 <107805747+Nithigna03@users.noreply.github.com>
2024-02-04 23:13:15 +0530

    Update mine.java

--- mine.java
+++ mine.java
@@ -2,6 +2,7 @@ public class mine{
 
     public static void main(String[] args){
         System.out.println("Java App Pulled & Build by Jenkins");
+        System.out.println("Java App Pulled");
         
     }
 }
